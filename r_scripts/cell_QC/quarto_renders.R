d03412b4c2591584ce0be141f1e4a0401968f99e
Valerio Reffo <valerio.reffo@gmail.com>
2026-08-06 15:53:14 +0200

    quarto-modular: sistemazione finale

--- r_scripts/cell_QC/quarto_renders.R
+++ r_scripts/cell_QC/quarto_renders.R
@@ -1,9 +1,13 @@
+library(purrr)
+library(quarto)
+
 datasets <- c("BE1", "cordblood", "sc_mixology")
+output_dir <- getwd() # or specify a folder like "~/scFound_bench/reports"
 
 purrr::walk(datasets, \(ds) {
   quarto::quarto_render(
     input = "~/scFound_bench/r_scripts/cell_QC/QC_main.qmd",
     execute_params = list(dataset = ds),
-    output_file = paste0("QC_", ds, ".html")
+    output_file = file.path(output_dir, paste0("QC_", ds, ".html"))
   )
 })
